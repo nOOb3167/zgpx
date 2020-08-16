8ad7a801fa4f10178a41e14f140e81f61f1a9f8a
nOOb3167 <nOOb3167@gmail.com>
2020-08-16 19:43:26 +0200

    stuff

--- pythonfu/gpxstitch.py
+++ pythonfu/gpxstitch.py
@@ -5,10 +5,9 @@
 # i0 = gimp.image_list()[0]
 
 from gimpfu import *
-import time
+import os, re, time
 
 DirRoot = 'C:/Users/Andrej/test/snw/maps/picos/'
-BufNam = 'gpxstitch_buffer'
 
 def exc(*args):
     gimp.message('ERR: ' + str(args))
@@ -23,18 +22,35 @@ def sel(img, x, y, w, h):
     pdb.gimp_image_select_rectangle(img, CHANNEL_OP_REPLACE, x, y, w, h)
 
 def cop(lay):
-    pdb.gimp_edit_named_copy(lay, BufNam)
+    pdb.gimp_edit_copy(lay)
 
 def pas(img, lay, x, y, w, h):
     sel(img, x, y, w, h)
-    pdb.gimp_floating_sel_anchor(pdb.gimp_edit_named_paste(lay, BufNam, True))
+    pdb.gimp_floating_sel_anchor(pdb.gimp_edit_paste(lay, True))
+
+def sorted_png_fnam(dir):
+    def fnam_sort(l):
+        def tryint(s):
+            try:
+                return int(s)
+            except:
+                return s
+        def key(s):
+            return [tryint(s) for s in re.split('([0-9]+)', s)]
+        return sorted(l, key=key)
+    fnam = [f for f in os.listdir(dir)]
+    return fnam_sort(filter(lambda x: re.match('.*\\.png$', x), fnam))
+
+def pairwise(img, dir):
+    fnam = sorted_png_fnam(dir)
+    for ix, fn in enumerate(fnam):
+        path = DirRoot + fn
+        i0 = pdb.gimp_file_load(path, path, run_mode=RUN_NONINTERACTIVE)
+        cop(img_lay(i0, 0))
+        pas(img, img_lay(img, 0), 32, 32+ix*128, 128, 128)
 
 def doit(img, lay):
-    FileName = DirRoot + '0.png'
-    i0 = pdb.gimp_file_load(FileName, FileName, run_mode=RUN_NONINTERACTIVE)
-    l0 = img_lay(i0, 0)
-    cop(l0)
-    pas(img, lay, 32, 32, 128, 128)
+    pairwise(img, DirRoot)
 
 def doit_(img, lay):
     gimp.message('S : ' + str(time.time()))
